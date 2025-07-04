89db76f549225a542c02ed1df0c35063492a1c1c
A-abd <65906361+A-abd@users.noreply.github.com>
2025-07-04 19:17:28 +0100

    feat: created get_all_collector_configs() which is an extensible method that currently only gets the wikipedia config, but will get all other configs in the future

--- predictai/common/config_loader.py
+++ predictai/common/config_loader.py
@@ -85,6 +85,21 @@ class ConfigLoader:
         return logging_config
     
     @staticmethod
+    def get_all_collector_configs():
+        """Gets all collector configs"""
+        
+        all_configs = {}
+        
+        try:
+            all_configs['wikipedia'] = ConfigLoader.get_wikipedia_collector_config()
+        except Exception as e:
+            print(f"Could not load wikipedia collector: '{e}'")
+            all_configs['wikipedia'] = None
+            
+        return all_configs
+            
+    
+    @staticmethod
     def get_test_pages_for_debug():
         """Get all pages for testing in the debug script"""
         wikipedia_config = ConfigLoader.get_wikipedia_collector_config()
