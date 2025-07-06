4417c15775943c465923f3b7ee31eb02b62dc8b7
A-abd <65906361+A-abd@users.noreply.github.com>
2025-07-06 21:05:27 +0100

    feat: created switch_language() which takes in a language code and sets wikipedia to that language. It only switches language.

--- predictai/common/wikipedia_utils.py
+++ predictai/common/wikipedia_utils.py
@@ -1,3 +1,5 @@
+import wikipedia
+
 from predictai.common.logging_config import get_logger
 
 class WikipediaUtils:
@@ -10,3 +12,20 @@ def __init__(
     
     self.config = config
     self.logger = get_logger(__name__)
+        
+def switch_language(
+    self,
+    language_code: str
+    ) -> bool:
+    """
+    Switches language for Wikipedia API. True if successful switch and vice versa.
+    """
+    
+    try: 
+        wikipedia.set_lang(language_code)
+        self.logger.info(f"Language successfully switched to '{language_code}'")
+        return True
+    
+    except Exception as e:
+        self.logger.error(f"Error switching to language: {language_code} - {e}")
+        return False
