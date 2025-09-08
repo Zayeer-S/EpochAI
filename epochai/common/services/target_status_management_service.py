c1579aee9d3ab28e98c80e484814ddb6ea27873d
A-abd <65906361+A-abd@users.noreply.github.com>
2025-09-08 15:18:57 +0100

    fix: correct wrong type in params

--- epochai/common/services/target_status_management_service.py
+++ epochai/common/services/target_status_management_service.py
@@ -1,7 +1,6 @@
 from epochai.common.database.dao.collection_statuses_dao import CollectionStatusesDAO
 from epochai.common.database.dao.collection_targets_dao import CollectionTargetsDAO
 from epochai.common.database.database import get_database
-from epochai.common.enums import CollectionStatusNames
 from epochai.common.logging_config import get_logger
 
 
@@ -17,15 +16,11 @@ class TargetStatusManagementService:
     def update_target_collection_status(
         self,
         collection_target_id: int,
-        collection_status_name: CollectionStatusNames,
+        collection_status_name: str,
     ) -> bool:
         """Updates a collection status to the passed in value"""
 
         try:
-            if not isinstance(collection_status_name, str):
-                self._logger.error(f"Passed in value not str: {collection_status_name}")
-                return False
-
             collection_status_id = self._collection_statuses_dao.get_id_by_name(collection_status_name)
             if not collection_status_id:
                 self._logger.error(f"Status ID for '{collection_status_name}' not found for target ID '{collection_target_id}'")
