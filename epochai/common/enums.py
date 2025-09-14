8e40ead731f87e4cc90f39a8e59eb011a9dd18f2
A-abd <65906361+A-abd@users.noreply.github.com>
2025-09-14 16:03:20 +0100

    feat: Create generic checker file to replace specific debug file

--- epochai/common/enums.py
+++ epochai/common/enums.py
@@ -3,12 +3,9 @@ from enum import Enum
 
 class CollectionStatusNames(Enum):
     NOT_COLLECTED = "not_collected"
-    IN_PROGRESS = "in_progress"
     COLLECTED = "collected"
     FAILED = "failed"
-    NEEDS_RETRY = "needs_retry"
-    SKIPPED = "skipped"
-    DEBUG_FAIL = "debug_failed"
+    CHECK_FAILED = "debug_failed"
 
 
 class AttemptStatusNames(Enum):
